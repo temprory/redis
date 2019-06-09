ff824fc594d3ccee654bc0fd079bef4682c93452
temprory <beishengren@126.com>
2019-06-09 19:10:17 +0800

    -

--- redis_test.go
+++ redis_test.go
@@ -1,41 +1,41 @@
 package redis
 
 import (
-	"sync"
-	"testing"
+	"fmt"
+	rds "github.com/go-redis/redis"
+	"github.com/temprory/redis"
 )
 
-func TestRedis(t *testing.T) {
-	conf := RedisConf{
-		Addr:              "127.0.0.1:6379",
+func main() {
+	conf := redis.RedisConf{
+		Addr:              "47.244.105.181:6379",
+		Password:          "QCzoikyY227",
 		Database:          0,
 		PoolSize:          10,
 		KeepaliveInterval: 2, //300
 	}
-	dbRedis := NewRedis(conf)
+	dbRedis := redis.NewRedis(conf)
 
-	wg := sync.WaitGroup{}
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 10000; j++ {
-				func() {
-					cmdset := dbRedis.Client().Set("test-key", "test-value", 0)
-					if cmdset.Err() != nil {
-						t.Fatal(cmdset.Err())
-					}
-
-					cmdget := dbRedis.Client().Get("test-key")
-					if cmdget.Err() != nil {
-						t.Fatal(cmdget.Err())
-					}
-					if ret, err := cmdget.Result(); err != nil {
-						t.Log("test-key:", ret, err)
-					}
-				}()
-			}
-		}()
+	cmdset := dbRedis.Client().Set("test-key", "test-value", 0)
+	if cmdset.Err() != nil {
+		t.Fatal(cmdset.Err())
 	}
-	wg.Wait()
+
+	dbRedis.Client().XAdd(&redis.XAddArgs{
+		Stream: "testStream",
+		ID:     "*",
+		Values: map[string]interface{}{"uno": "un"},
+	})
+
+	rets, err := dbRedis.Client().XRead(&redis.XAddArgs{
+		Stream: "testStream",
+		ID:     "*",
+		Values: map[string]interface{}{"uno": "un"},
+	}).Result()
+	fmt.Println("--- err:", err, len(rets))
+	for i, v := range rets {
+		for j, v := range v.Messages {
+			fmt.Println(i, j, v.Messages[0].Values)
+		}
+	}
 }
